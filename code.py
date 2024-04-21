ee870e821c85a69597a0ae6afad3c2a86f38ddd8
sahrabullah7 <145543132+sahrabullah7@users.noreply.github.com>
2024-04-22 01:32:08 +0200

    Update code.py

--- code.py
+++ code.py
@@ -24,3 +24,18 @@ def large_prime(bits):
         q = random.randint(2**(bits-1), 2**(bits))    # generate the 2nd prime no
         if prime(p) and prime(q) and p != q:
             return p, q
+
+# Greatest common divisor
+def gcd(a, b):                                      
+    while b != 0:
+        a, b = b, a % b                               # replace a with the b, replace b with the remainder
+    return a 
+
+# Extended greatest common divisor
+def extended_gcd(a, b):                             
+    x0, x1, y0, y1 = 1, 0, 0, 1
+    while b:
+        q, a, b = a // b, b, a % b
+        x0, x1 = x1, x0 - q * x1
+        y0, y1 = y1, y0 - q * y1
+    return a, x0, y0   
